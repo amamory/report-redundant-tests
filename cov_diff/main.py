7bc43b9e964bf26ec78b037ee80c0554e9a1cc74
De Morais Amory Alexandre (ETAS-DAP/ENG-P) <Alexandre.DeMoraisAmory@etas.com>
2023-12-10 17:16:08 +0100

    script is running the tests

--- cov_diff/main.py
+++ cov_diff/main.py
@@ -18,6 +18,7 @@ import shutil
 # GreaterTest.BisGreater
 # GreaterTest.CisGreater
 def generate_gtest_list(test_exec):
+    # https://stackoverflow.com/questions/24849998/how-to-catch-exception-output-from-python-subprocess-check-output
     cmd=test_exec + " --gtest_list_tests | awk '/\./{ SUITE=$1 }  /  / { print SUITE $1 }'"
     result = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)
     output = result.decode("utf-8",'ignore')
@@ -27,38 +28,69 @@ def generate_gtest_list(test_exec):
     return all_tests_list
 
 # run the test to generate .gcda file w coverage data and then run gcovr to generate the coverage report in txt format
-# when test_exclude is not empty, it excludes the test, otherwise, it runs all tests
+# when 'test_name' is not empty, it excludes the test, otherwise, it runs all tests
+# 'test_filter_mode' is either '-' or ''. The former means a inverted filter, i.e., all tests except for 'test_name'
 # it assumes the current folder is the build folder
-def run_gcovr(test_exec, test_exclude):
-    #TODO check if this is the build folder
-    print ("Running test", test_exec, test_exclude)
-    if test_exclude:
-        cmd = test_exec
+def run_gcovr(test_exec, test_name, test_filter_mode, args):
+
+    print ("Running test", test_exec, test_name)
+    if not os.path.isfile(test_exec):
+        print ("ERROR: Test executable", test_exec, "not found")
+        sys.exit(1)
+        
+    # empty test_name means it's running the entire test set
+    if test_name:
+        cmd = test_exec + " --gtest_filter='"+ test_filter_mode + test_name + "'"
     else:
-        cmd = test_exec + " --gtest_filter='-" + test_exclude + "'"
+        cmd = test_exec
     result = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)
+    if not args.quiet:
+        print( result.decode("utf-8",'ignore') )
     # TODO check for errors
     cmd = "make coverage_txt"
+    # the 1st filter mode means all tests except for test_name
+    # the 2nd filter mode means only test_name
     result = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)
-    # TODO check for errors
     if not os.path.isfile("./coverage_txt/coverage.txt"):
-        print ("ERROR: ./coverage_txt/coverage.txt not generated for:", test_exec, test_exclude)
+        print ("ERROR: ./coverage_txt/coverage.txt not generated for:", test_exec, test_name)
         sys.exit(1)
+    # TODO check for errors
+
+    if not args.quiet:
+        print( result.decode("utf-8",'ignore') )
+        with open('./coverage_txt/coverage.txt') as f:
+            content = f.read()
+            print (content)
     # move the coverage report to avoid being overwritten
-    os.makedirs(os.path.dirname("../cov_reports/"), exist_ok=True)
-    if test_exclude:
-        shutil.move("./coverage_txt/coverage.txt", "../cov_reports/"+test_exec+".txt")
+    if test_name:
+        dst_file = os.path.join(args.report_dir, test_name+test_filter_mode+".txt")
     else:
-        shutil.move("./coverage_txt/coverage.txt", "../cov_reports/"+test_exclude+".txt")
-    print ("Test", test_exec, test_exclude, "executed!")
+        dst_file = os.path.join(args.report_dir, test_exec+".txt")
+    # if os.path.exists(dst_file):
+    #     os.remove(dst_file)
+    shutil.move("./coverage_txt/coverage.txt", dst_file)
+    print ("Test", test_exec, test_name+test_filter_mode, "executed!")
 
 
 def main():
     parser = argparse.ArgumentParser(description='Shows the number of lines each test is contributing, compared to the entire test set.')
     parser.add_argument('--test-app', type=str, required=True, help='GTest executable')
+    parser.add_argument('--exec-dir', type=str, required=True, help='Directory where the tests must be launched')
+    parser.add_argument('--report-dir', type=str, default="../cov_reports", help='Directory where the coverage reports are saved')
+    parser.add_argument('--skip-tests', action='store_true', help='Skip the test execution, assuming the reports were already generated (optional).')
+    parser.add_argument('--find-subset-tests', action='store_true', help='Additional check to find tests that are a subset of other tests (optional).')
+    parser.add_argument('--quiet', action='store_true', help='Reduce verbosity (optional).')
     parser.add_argument('--summary', action='store_true', help='Report summary (optional).')
 
     args = parser.parse_args()
+
+    try:
+        os.chdir(args.exec_dir)
+    except FileNotFoundError:
+        print(args.exec_dir + ' does NOT exist. Aborting')
+        sys.exit(1)
+
+    os.makedirs(os.path.dirname(args.report_dir), exist_ok=True)
 
     # Access the parsed arguments
     test_exec = args.test_app
@@ -70,14 +102,20 @@ def main():
     print (test_list)
     print (type(test_list))
 
-    #2) run the full test to get it's missed lines
-    # save the report in gcovr txt format w the name <test_executable>.txt
-    run_gcovr(test_exec, "")
+    if not args.skip_tests:
+        #2) run the full test to get it's missed lines
+        # save the report in gcovr txt format w the name <test_executable>.txt
+        run_gcovr(test_exec, "", "", args)
 
-    #3) iterate on each test, excluding the test from the execution 
-    # save the report in gcovr txt format w the name <test_name>.txt
-    for test in test_list:
-        run_gcovr(test_exec, test)
+        #3) iterate on each test, excluding the test from the execution 
+        # save the report in gcovr txt format w the name <test_name><filter_mode>.txt
+        filter_modes = ['-']
+        if args.find_subset_tests:
+            filter_modes.append('')
+        for test in test_list:
+            for fmode in filter_modes:
+                run_gcovr(test_exec, test, fmode, args)
+        sys.exit(1)
 
     #4) parse the gcovr txt reports to gather the missed lines for every test
 
